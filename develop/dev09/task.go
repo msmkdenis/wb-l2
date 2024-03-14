9479400da73564bd7e0aa83b2f7d0ef29f9d4da3
Denis Burtsev <msmkdenis@gmail.com>
2024-03-14 11:30:49 +0300

    dev09

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+)
+
 /*
 === Утилита wget ===
 
@@ -8,6 +19,55 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+const (
+	errArg = "check for argument correctnes"
+)
 
+func main() {
+	uri := flag.String("s", "", "Uniform Resource Identifier, specify base url for successful download of the site")
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		fmt.Println(errArg)
+		return
+	}
+
+	if ok, err := regexp.MatchString("^(http|https)://", *uri); !ok || err != nil {
+		fmt.Println("invalid url")
+		return
+	}
+
+	if err := wget(*uri); err != nil {
+		log.Fatalf("[Error] %v\n", err)
+	}
+
+}
+
+// wget func - write resualts of a GET request to a file.
+func wget(url string) error {
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	file, err := os.Create("index.html")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	_, err = io.Copy(writer, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
 }
